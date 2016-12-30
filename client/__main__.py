37ae30a2ff163f697b2d74d7f22fdc4ff22719d8
robertdfrench <frenchrd@ornl.gov>
2016-12-30 15:34:10 -0500

    Run in a loop

--- client/__main__.py
+++ client/__main__.py
@@ -1,10 +1,16 @@
 #!/bin/env python
+import time
 from client.util import shell
 from client import push_event
 from client import run_ci
 
-pe = push_event.next()
-if pe:
-    run_ci.run(pe)
-else:
-    print("No push events available right now")
+def try_ci():
+    pe = push_event.next()
+    if pe:
+        run_ci.run(pe)
+    else:
+        print("No push events available right now")
+
+while True:
+    try_ci()
+    time.sleep(10)
